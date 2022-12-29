dc389f914ac7c5de9fbb76fde70b044e434e25b1
Connor Jacobsen <jacobsen.connor@gmail.com>
2022-12-28 22:07:58 -0800

    add loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 // Uncomment this block to pass the first stage
-use std::{net::TcpListener, io::Write};
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    loop {
+        println!("Sending PONG...");
+        stream.write(b"+PONG\r\n").expect("Woo!");
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,9 +19,8 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("Sending PONG...");
-                stream.write(b"+PONG\r\n").expect("Woo!");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
