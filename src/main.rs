08252a72236c789c32aadb0bfdd663bf3c9f420b
Connor Jacobsen <jacobsen.connor@gmail.com>
2025-12-22 22:04:45 -0800

    implement ECHO

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,57 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use anyhow::Result;
+use connection::Value;
+use tokio::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 512];
+mod connection;
+
+pub async fn handle_connection(stream: TcpStream) {
+    let mut handler = connection::ConnectionHandler::new(stream);
 
     loop {
-        // Wait for the client to send us a message.
-        let bytes_read = stream.read(&mut buf).unwrap();
-        if bytes_read == 0 {
-            println!("Client closed the connection");
-            break;
-        }
+        let value = handler.read_value().await.unwrap();
 
-        stream.write(b"+PONG\r\n").expect("Woo!");
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
+                c => panic!("Cannot handle command {}", c),
+            }
+        } else {
+            break;
+        };
+
+        handler.write_value(response).await.unwrap();
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
+    match value {
+        Value::Array(a) => Ok((
+            unpack_bulk_str(a.first().unwrap().clone())?,
+            a.into_iter().skip(1).collect(),
+        )),
+        _ => Err(anyhow::anyhow!("Unexpected command format")),
+    }
+}
 
-    for stream in listener.incoming() {
+fn unpack_bulk_str(value: Value) -> Result<String> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("Expected command to be a bulk string")),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(stream) => {
-                thread::spawn(|| handle_connection(stream));
+            Ok((stream, _)) => {
+                tokio::spawn(async move { handle_connection(stream).await });
             }
             Err(e) => {
                 println!("error: {}", e);
